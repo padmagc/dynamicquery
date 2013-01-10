174ba9846fae9148792fffce80acf6e17aa065df
richardgaspar79@gmail.com <richardgaspar79@gmail.com@4e59c685-324e-4ea9-9719-32890e2ea03e>
2013-01-10 16:43:53 +0000

    Added a new project which generate a xml file from the database of documentation

---
+++ trunk/Source/DataAccess/Documentation/DynamicQuery.GenerateDocumentationXML/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DynamicQuery.Entity.Documentation;
+
+namespace DynamicQuery.GenerateDocumentationXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database db = new Database();
+            
+            System.Xml.Serialization.XmlSerializer writer = new System.Xml.Serialization.XmlSerializer(db.GetType());
+            
+            System.IO.StreamWriter file = new System.IO.StreamWriter("QBDatabaseDocumentation.xml");
+
+            writer.Serialize(file, db);
+            file.Close();
+        }
+    }
+}
